0b4eb565b73fd476b8a444014168d10f4747e34d
Draemiz <draemizgx1@gmail.com>
2026-05-26 20:27:50 -0500

    MAMM: crear entidad Vuelo con mapeo JPA

---
+++ vuelos/src/main/java/com/krakedev/examen/vuelos/entities/Vuelo.java
@@ -0,0 +1,57 @@
+package com.krakedev.examen.vuelos.entities;
+
+import jakarta.persistence.*;
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "vuelos")
+public class Vuelo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "codigo", length = 10, nullable = false, unique = true)
+    private String codigo;
+
+    @Column(name = "precio_boleto", nullable = false)
+    private BigDecimal precioBoleto;
+
+    @Column(name = "asientos_disponibles", nullable = false)
+    private Integer asientosDisponibles;
+
+    public Vuelo() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public BigDecimal getPrecioBoleto() {
+        return precioBoleto;
+    }
+
+    public void setPrecioBoleto(BigDecimal precioBoleto) {
+        this.precioBoleto = precioBoleto;
+    }
+
+    public Integer getAsientosDisponibles() {
+        return asientosDisponibles;
+    }
+
+    public void setAsientosDisponibles(Integer asientosDisponibles) {
+        this.asientosDisponibles = asientosDisponibles;
+    }
+}
